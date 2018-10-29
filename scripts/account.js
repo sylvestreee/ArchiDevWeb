1f0cb85f1b54f488bff6295ae234a3152a91e56e
sylvestreee <martin-heitz@orange.fr>
2018-10-29 13:19:43 +0100

    improve form validation in sign / account

--- scripts/account.js
+++ scripts/account.js
@@ -11,6 +11,7 @@ $(document).ready(function() {
     var no_blank = input.val().length > 0;
     var verif_email=/^[^\W][a-zA-Z0-9_-]+(\.[a-zA-Z0-9_-]+)*\@[a-zA-Z0-9_]+(\.[a-zA-Z0-9_]+)*\.[a-zA-Z]{2,4}$/;
     var verif_pwd=/^(?=.*\d)(?=.*[A-Z]).{6,}$/;
+    var verif_date=/^((((0[1-9]|[12][0-9]|3[01])[- /.](0[13578]|1[02]))|((0[1-9]|[12][0-9]|30)[- /.](0[469]|11))|((0[1-9]|[12][0-8])[- /.](02)))[- /.]\d{4})|((0[1-9]|[12][0-9])[- /.](02)[- /.](([0-9]{2}(04|08|[2468][048]|[13579][26])|2000)))$/;
 
     //checkbox input
     if(input.hasClass("check-1") || input.hasClass("check-2") || input.hasClass("check-3")) {
@@ -110,6 +111,33 @@ $(document).ready(function() {
             }
           }
         }
+
+        //birthday input
+        else if(input.hasClass("birthday-input")) {
+
+          //valid
+          if(verif_date.test(input.val())) {
+            if(!input.hasClass("valid")) {
+              input.removeClass("invalid").addClass("valid");
+              $('.thumbs-birthday').removeClass("visible").addClass("invisible");
+              $('.warning-birthday').removeClass("visible").addClass("invisible");
+              nb_input++;
+            }
+          }
+
+          //not valid
+          else {
+            $('.birthday-input').removeClass("valid").addClass("invalid");
+            $('.thumbs-birthday').removeClass("invisible").addClass("visible");
+            $('.warning-birthday').removeClass("invisible").addClass("visible");
+            if(nb_input != 0) {
+              nb_input--;
+            }
+          }
+
+
+        }
+
         else {
           if(!input.hasClass("valid")) {
             input.removeClass("invalid").addClass("valid");
@@ -124,6 +152,16 @@ $(document).ready(function() {
           input.removeClass("valid").addClass("invalid");
           $("i", $('.email').parent()).removeClass("visible").addClass("invisible");
           $("span", $('.email').parent()).removeClass("visible").addClass("invisible");
+          if(nb_input != 0) {
+            nb_input--;
+          }
+        }
+
+        //birthday input
+        else if(input.hasClass("birthday-input")) {
+          input.removeClass("valid").addClass("invalid");
+          $('.thumbs-birthday').removeClass("visible").addClass("invisible");
+          $('.warning-birthday').removeClass("visible").addClass("invisible");
           if(nb_input != 0) {
             nb_input--;
           }
