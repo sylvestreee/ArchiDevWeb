c521769bf9d6cb2911856c6c7cf0eb576114b652
sylvestreee <martin-heitz@orange.fr>
2018-10-29 15:52:25 +0100

    improve form validation in account

--- scripts/account.js
+++ scripts/account.js
@@ -12,6 +12,7 @@ $(document).ready(function() {
     var verif_email=/^[^\W][a-zA-Z0-9_-]+(\.[a-zA-Z0-9_-]+)*\@[a-zA-Z0-9_]+(\.[a-zA-Z0-9_]+)*\.[a-zA-Z]{2,4}$/;
     var verif_pwd=/^(?=.*\d)(?=.*[A-Z]).{6,}$/;
     var verif_date=/^((((0[1-9]|[12][0-9]|3[01])[- /.](0[13578]|1[02]))|((0[1-9]|[12][0-9]|30)[- /.](0[469]|11))|((0[1-9]|[12][0-8])[- /.](02)))[- /.]\d{4})|((0[1-9]|[12][0-9])[- /.](02)[- /.](([0-9]{2}(04|08|[2468][048]|[13579][26])|2000)))$/;
+    var verif_name=/^[a-zA-Z -]*[^0-9]$/;
 
     //checkbox input
     if(input.hasClass("check-1") || input.hasClass("check-2") || input.hasClass("check-3")) {
@@ -86,10 +87,67 @@ $(document).ready(function() {
       }
     }
     else {
+
+      //input's not empty
       if(no_blank) {
 
+        if(input.hasClass("pseudo")) {
+          if(!input.hasClass("valid")) {
+            input.removeClass("invalid").addClass("valid");
+            nb_input++;
+          }
+        }
+
+        //name input
+        else if(input.hasClass("name")) {
+
+          //valid
+          if(verif_name.test(input.val())) {
+            if(!input.hasClass("valid")) {
+              input.removeClass("invalid").addClass("valid");
+              $("i", $('.name').parent()).removeClass("visible").addClass("invisible");
+              $("span", $('.name').parent()).removeClass("visible").addClass("invisible");
+              nb_input++;
+            }
+          }
+
+          //not valid
+          else {
+            $('.name').removeClass("valid").addClass("invalid");
+            $("i", $('.name').parent()).removeClass("invisible").addClass("visible");
+            $("span", $('.name').parent()).removeClass("invisible").addClass("visible");
+            if(nb_input != 0) {
+              nb_input--;
+            }
+          }
+        }
+
+        //fname input
+        else if(input.hasClass("fname")) {
+
+          //valid
+          if(verif_name.test(input.val())) {
+            if(!input.hasClass("valid")) {
+              input.removeClass("invalid").addClass("valid");
+              $("i", $('.fname').parent()).removeClass("visible").addClass("invisible");
+              $("span", $('.fname').parent()).removeClass("visible").addClass("invisible");
+              nb_input++;
+            }
+          }
+
+          //not valid
+          else {
+            $('.fname').removeClass("valid").addClass("invalid");
+            $("i", $('.fname').parent()).removeClass("invisible").addClass("visible");
+            $("span", $('.fname').parent()).removeClass("invisible").addClass("visible");
+            if(nb_input != 0) {
+              nb_input--;
+            }
+          }
+        }
+
         //email input
-        if(input.hasClass("email")) {
+        else if(input.hasClass("email")) {
 
           //valid
           if(verif_email.test(input.val())) {
@@ -134,8 +192,6 @@ $(document).ready(function() {
               nb_input--;
             }
           }
-
-
         }
 
         else {
@@ -145,10 +201,40 @@ $(document).ready(function() {
           }
         }
       }
+
+      //input's empty
       else {
 
+        //pseudo input
+        if(input.hasClass("pseudo")) {
+          input.removeClass("valid").addClass("invalid");
+          if(nb_input != 0) {
+            nb_input--;
+          }
+        }
+
+        //name input
+        else if(input.hasClass("name")) {
+          input.removeClass("valid").addClass("invalid");
+          $("i", $('.name').parent()).removeClass("visible").addClass("invisible");
+          $("span", $('.name').parent()).removeClass("visible").addClass("invisible");
+          if(nb_input != 0) {
+            nb_input--;
+          }
+        }
+
+        //fname input
+        else if(input.hasClass("fname")) {
+          input.removeClass("valid").addClass("invalid");
+          $("i", $('.fname').parent()).removeClass("visible").addClass("invisible");
+          $("span", $('.fname').parent()).removeClass("visible").addClass("invisible");
+          if(nb_input != 0) {
+            nb_input--;
+          }
+        }
+
         //email input
-        if(input.hasClass("email")) {
+        else if(input.hasClass("email")) {
           input.removeClass("valid").addClass("invalid");
           $("i", $('.email').parent()).removeClass("visible").addClass("invisible");
           $("span", $('.email').parent()).removeClass("visible").addClass("invisible");
