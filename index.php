5642b13eec5b9b4b36c8258f3b28df775f1c4660
sylvestreee <martin-heitz@orange.fr>
2018-11-06 23:00:12 +0100

    starting implementation of MVC model

--- index.php
+++ index.php
@@ -1,7 +1,27 @@
 <?php
 
-require "global/header.html";
+$url = $_GET['url'];
+$file = basename($url);
+
+require 'global/header.html';
+
+switch($file) {
+  case "home" :
+    require 'controllers/home.html';
+    break;
+  case "catalogue" :
+    require 'controllers/catalogue.html';
+    break;
+  case "log" :
+    require 'controllers/log.html';
+    break;
+  case "sign" :
+    require 'controllers/sign.html';
+    break;
+  default :
+    break;
+}
 
 require 'global/footer.html';
 
-?>
+>
