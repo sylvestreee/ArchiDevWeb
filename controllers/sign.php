0dce87016201edfdf02ea9306112d3c7a6e0f662
Martin HEITZ <martin.heitz@etu.unistra.fr>
2018-12-27 17:23:02 +0000

    user session start

--- controllers/sign.php
+++ controllers/sign.php
@@ -1,5 +1,30 @@
 <?php
 
-$template = $twig->load('sign.twig');
+namespace Website\Controllers;
 
-echo $template->render();
+global $twig;
+global $entityManager;
+
+class Sign {
+    private $twig;
+    
+    public function __construct() {
+        global $twig;
+        global $entityManager;
+        
+        $this->twig = $twig;
+        $this->entityManager = $entityManager;
+    }
+    
+    public function index() {
+        $template = $this->twig->load('sign.twig');
+        echo $template->render();
+    }
+    
+    public function connexion($post) {
+        $_SESSION['login'] = $post['email'];
+        $_SESSION['pseudo'] = "sylvestreee";
+        $template = $this->twig->load('account.twig');
+        echo $template->render();
+    }
+}
--- index.php
+++ index.php
@@ -1,10 +1,14 @@
 <?php
 
+session_start();
+// session_destroy();
+
 require_once './vendor/autoload.php';
 require_once './bootstrap.php';
 
 $loader = new Twig_Loader_Filesystem('./views');
 $twig   = new Twig_Environment($loader);
+$twig->addGlobal("session", $_SESSION);
 
 $url    = $_SERVER['REQUEST_URI'];
 
@@ -84,21 +88,39 @@ try {
           
         /*sign*/
         case "sign" :
-          require './controllers/sign.php';
+          $signController = new Website\Controllers\sign();
+          
+          if($method == NULL) {
+            $signController->index();
+          }
+          
+          else {
+            
+            /*if the user did a research in the navbar*/
+            switch($method) {
+              case "connexion" :
+                $signController->connexion($_POST);
+                break;
+              
+              default :
+                throw new Exception();
+                break;
+            }
+          }
           break;
           
         /*user*/
-        case "/user" :
+        case "user" :
           require './controllers/user.php';
           break;
   
         /*account*/
-        case "/account" :
+        case "account" :
           require './controllers/account.php';
           break;
           
         /*cart*/
-        case "/cart" :
+        case "cart" :
           require './controllers/cart.php';
           break;
           
@@ -114,50 +136,3 @@ try {
   $template = $twig->load('error.twig');
   echo $template->render();
 }
-//   else if($controller == '') {
-//     $homeController = new Website\Controllers\home();
-//     $homeController->index();
-//   }
-//   else {
-//     throw new Exception();
-//   }
-// } catch (Exception $e) {
-//   $template = $twig->load('error.twig');
-//   echo $template->render();
-// }
-
-
-// switch($url) {
-//   case "/" :
-//   case "/index.php" :
-//   case "/home" :
-//     $homeController = new Website\Controllers\home();
-//     $homeController->index();
-//     break;
-//   case "/catalogue" :
-//     $catalogueController = new Website\Controllers\catalogue();
-//     $catalogueController->index();
-//     break;
-//   case "/log" :
-//     require './controllers/log.php';
-//     break;
-//   case "/sign" :
-//     require './controllers/sign.php';
-//     echo "hey";
-//     break;
-//   case "/game" :
-//   	require './controllers/game.php';
-//   	break;
-//   case "/user" :
-//     require './controllers/user.php';
-//     break;
-//   case "/account" :
-//     require './controllers/account.php';
-//     break;
-//   case "/cart" :
-//     require './controllers/cart.php';
-//     break;
-//   default :
-//   	require './controllers/error.php';
-//     break;
-// }
